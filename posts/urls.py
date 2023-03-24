82130cacfbeddd12022e1a485db090aac7589311
unknown <jaewoniik94@gmail.com>
2023-03-24 15:12:53 +0900

    Feat : 게시글 목록 조회 기능 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = "posts"
+
 urlpatterns = [
     path("", views.PostListView.as_view(), name="post_list"),
     path("upload", views.PostUploadView.as_view(), name="post_upload"),
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Post, Comment
 
 
 class PostListView(generic.ListView):
-    pass
+    model = Post
+    template_name = "home.html"
 
 
 class PostUploadView(generic.FormView):
