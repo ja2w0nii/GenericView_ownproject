ca32940bf2b2cddfabb36ab3fa5b0c527559755b
unknown <jaewoniik94@gmail.com>
2023-03-27 11:02:00 +0900

    Style : 에러 문구 변경

--- posts/urls.py
+++ posts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "posts"
 
 urlpatterns = [
+    # 게시글 CRUD
     path("", views.PostListView.as_view(), name="post_list"),
     path("upload/", views.PostUploadView.as_view(), name="post_upload"),
     path("<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
