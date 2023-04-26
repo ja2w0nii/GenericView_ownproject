c911874953f8c5cae3cffda7feaff9db1e677437
unknown <jaewoniik94@gmail.com>
2023-04-26 11:18:44 +0900

    Style: 모든 페이지에 base.html 적용

--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,7 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ("email", "nickname", "profile_img")
+        fields = ("email", "nickname")
 
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
