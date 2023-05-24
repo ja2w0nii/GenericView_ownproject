826a6cccbb3a79734efc95a2ae630e4ef98c0493
unknown <jaewoniik94@gmail.com>
2023-05-24 17:49:57 +0900

    Style: 회원가입 페이지 스타일 변경

--- users/admin.py
+++ users/admin.py
@@ -8,23 +8,24 @@ from users.models import User
 
 
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="비밀번호", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="비밀번호 확인", widget=forms.PasswordInput)
+    email = forms.EmailField(label="이메일")
+    password = forms.CharField(label="비밀번호", widget=forms.PasswordInput)
+    password_check = forms.CharField(label="비밀번호 확인", widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ("email",)
 
     def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
+        password = self.cleaned_data.get("password")
+        password_check = self.cleaned_data.get("password_check")
+        if password and password_check and password != password_check:
             raise ValidationError("Passwords don't match")
-        return password2
+        return password_check
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        user.set_password(self.cleaned_data["password"])
         if commit:
             user.save()
         return user
